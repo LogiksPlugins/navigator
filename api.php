34be4daede97310d6616f8126c36d7fd0c459d4b
Bismay <bismay4u@gmail.com>
2016-03-13 04:48:13 +0530

    Upgraded the navigator module to 2.0 with support for Logiks 4.0 Framework.

--- api.php
+++ api.php
@@ -1,87 +1,20 @@
 <?php
 if(!defined('ROOT')) exit('No direct script access allowed');
 
-if(!function_exists("getNav")) {
-	include_once dirname(__FILE__)."/AutoMenus.inc";
+if(!function_exists("generateNavigationFromDB")) {
 
-	function getNav($menuid,$menuAutoGroupFile=null,$params=null,$colDefn=null) {
-		$treeArray=array();
+	include __DIR__."/MenuGenerator.inc";
 
-		if($colDefn==null) {
-			$colDefn=array(
-				"idCol"=>"id",
-				"titleCol"=>"title",
-				"groupCol"=>"menugroup",
-				"categoryCol"=>"category",
-				"linkCol"=>"link",
-				"iconCol"=>"iconpath",
-				"classCol"=>"class",
-				"tipsCol"=>"tips",
-				"targetCol"=>"target",
-				"toCheckCol"=>"to_check",
-			);
-		}
-		if($params==null) {
-			$params=array(
-					//"printTitle"=>true,
-					"orderBy"=>null,
-					"dbtable"=>_dbtable("links"),
-					"requiredTableList"=>_db(true)->getTableList(),
-					"requiredModuleList"=>null,
-					"sysdb"=>false,
-					"site"=>SITENAME,
-				);
-		}
-
-		$sm=getNavMenuObj($menuid,$menuAutoGroupFile,$params,$colDefn);
-
-		$out=$sm->getMainMenuArray($params['dbtable'],$menuid,$params['site'],$_SESSION["SESS_PRIVILEGE_NAME"],$colDefn,$params['orderBy'],$sysdb);
-		$arr=$sm->getSubmenuArrays($params['sysdb']);
-
-		foreach($out as $a=>$b) {
-			$treeArray=array_merge_recursive($treeArray,$b);
-		}
-		foreach($arr as $a=>$b) {
-			$treeArray=array_merge_recursive($treeArray,$b);
-		}
-		return $treeArray;
+	//Based on Data Tables, create menu and implement the menu system (TODO cache)
+	function generateNavigationFromDB($navKey="default",$dbTable=null,$dbKey="app") {
+		$mg=new MenuGenerator("Masters",['core',$dbKey]);
+		return $mg->processMenuArray($mg->processMenuDB($navKey,$dbTable,$dbKey));
 	}
-	function getNavMenuObj($menuid,$menuAutoGroupFile=null,$params=null) {
-		if($params==null) {
-			$params=array(
-					//"printTitle"=>true,
-					"dbtable"=>_dbtable("links"),
-					"requiredTableList"=>_db(true)->getTableList(),
-					"requiredModuleList"=>null,
-					"sysdb"=>false,
-					"site"=>SITENAME,
-				);
-		}
 
-		if($menuAutoGroupFile!=null) {
-			if(file_exists($menuAutoGroupFile)) {
-				$json=file_get_contents($menuAutoGroupFile);
-				$arrMenu=json_decode($json, true);
-				if($arrMenu==null) $arrMenu=array();
-				else {
-					foreach($arrMenu as $a=>$b) {
-						if(isset($b['enabled']) && !$b['enabled'] && !$allGenerators) {
-							unset($arrMenu[$a]);
-						}
-					}
-				}
-			}
-		} else {
-			$arrMenu=array();
-		}
-
-		$sm=new AutoMenus($arrMenu);
-		//$sm->printTitle($params['printTitle']);
-		$sm->requiredTableList($params['requiredTableList']);
-		$sm->requiredModuleList($params['requiredModuleList']);
-		$sm->generateSQL($params['site'],$_SESSION["SESS_PRIVILEGE_NAME"],$params['sysdb']);
-		
-		return $sm;
+	//Based on JSON files in the provided folder, create menu and implement the menu system (TODO cache)
+	function generateNavigationFromDir($dirPath,$dbKey="app") {
+		$mg=new MenuGenerator("Masters",['core',$dbKey]);
+		return $mg->processMenuArray($mg->processMenuDir($dirPath));
 	}
 }
 ?>
